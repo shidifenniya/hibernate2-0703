cbcc9bb8696e865a05e741df7410400b21261fe5
shidifenniya <shidifenniya@outlook.com>
2017-10-18 19:07:12 +0800

    绑定关系
    作者 : 最终变身的小黑
    
    时间 : 2017-10-18   5:21

---
+++ src/com/lanou/domain/Manager.java
@@ -0,0 +1,60 @@
+package com.lanou.domain;
+
+/**
+ * Created by dllo on 17/10/18.
+ */
+public class Manager {
+
+    private int id;
+
+    private String name;
+
+    private Department department;
+
+
+    public Manager(int id, String name, Department department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    public Manager() {
+    }
+
+    public Manager(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department=" + department +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
